3b9e6bfec84977e04155ebbfd00b8bb3221eeb94
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-11 00:16:54 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -7,9 +7,20 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestLine = data.toString().split("\r\n")[0];
     const [, path] = requestLine.split(" ");
-    const response = path === "/"
-      ? "HTTP/1.1 200 OK\r\n\r\n"
-      : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    let response;
+
+    if (path.startsWith("/echo/")) {
+      const echoValue = path.slice("/echo/".length);
+      const body = echoValue;
+      const contentLength = Buffer.byteLength(body, "utf8");
+
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${body}`;
+    } else if (path === "/") {
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else {
+      response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
 
     socket.write(response);
     socket.end();
