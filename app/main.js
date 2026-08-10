555819c9ee3a52b8719bb8f617f7ef99c8b3ee8e
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-11 00:13:17 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -4,8 +4,14 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const requestLine = data.toString().split("\r\n")[0];
+    const [, path] = requestLine.split(" ");
+    const response = path === "/"
+      ? "HTTP/1.1 200 OK\r\n\r\n"
+      : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    socket.write(response);
     socket.end();
   });
 });
